b96fb8f0478f3914a9e2df521435684e89513049
unknown <magedfiverr@gmail.com>
2024-08-03 18:20:47 +0200

    create add endpoint

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,10 +9,55 @@ POSTS = [
     {"id": 2, "title": "Second post", "content": "This is the second post."},
 ]
 
+def generate_id(storage):
+    '''Get a list of dict and sum 1 to the latest id and returns it'''
+    max_id = 0
+    for data in storage:
+        if max_id < data["id"]:
+            max_id = data["id"]
+    return max_id + 1
+
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+    post_json = request.get_json()
+    
+    # Validate Data
+    if "title" not in post_json and "content" not in post_json:
+        response = {
+            "error": "Bad Request",
+            "message": "Missing fields: 'title', 'content'"
+        }
+        return response
+    
+    elif "title" not in post_json:
+        response = {
+            "error": "Bad Request",
+            "message": "Missing fields: 'title'"
+        }
+        return response
+    
+    elif "content" not in post_json:
+        response = {
+            "error": "Bad Request",
+            "message": "Missing fields: 'content'"
+        }
+        return response
+    
+    
+
+
+    #add new data to storage
+    id = generate_id(POSTS)
+    new_data = {"id": id, "title": post_json["title"], "content": post_json["content"]}
+    POSTS.append(new_data)
+    return jsonify(POSTS)
+
 
 
 if __name__ == '__main__':
